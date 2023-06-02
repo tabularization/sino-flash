9ee82132b90c5465f967c1f847dd2c181c2dc61f
tabularization <127825421+tabularization@users.noreply.github.com>
2023-06-02 15:19:19 -0400

    Implemented OOP approach
    Instead of using global variables, initialized a class object to organize code.

--- main.py
+++ main.py
@@ -2,68 +2,63 @@ from tkinter import *
 import pandas 
 import random
 
-BLONDE = "#F9FBE7"
-BEIGE = "#F0EDD4"
 ORANGE = "#ECCDB4"
-SALMON = "#FEA1A1"
-RANDOM_WORD = ""
-MANDARIN_WORD = ""
-ENGLISH_WORD = ""
 
-# Reading data
-data = pandas.read_csv("data/mandarin_words.csv")
-data = data.to_dict(orient='records')
-RANDOM_WORD = random.choice(data) # Not the best way to do this
+class FlashcardApp:
+    def __init__(self):
+        self.data = self.load_data()
+        self.random_word = random.choice(self.data)
+        self.mandarin_word = ""
+        self.english_word = ""
 
-# ---------------------------- FUNCTIONS ------------------------------- #
-def correct():
-    global MANDARIN_WORD
-    update_flashcard()
-    print(MANDARIN_WORD)
-     
-def update_flashcard():
-    global MANDARIN_WORD
-    global RANDOM_WORD
-    flashcard.itemconfig(flashcard_image, image=front_image)
-    flashcard.itemconfig(flashcard_language, text="Mandarin", fill="Black")
-    RANDOM_WORD = random.choice(data)
-    MANDARIN_WORD = RANDOM_WORD['Mandarin']
-    flashcard.itemconfig(flashcard_text, text=MANDARIN_WORD, fill="Black")
+        self.window = Tk()
+        self.window.title("Sino Flashcards")
+        self.window.config(padx=50, pady=50, bg=ORANGE)
+        
+        self.flashcard = Canvas(width=800, height=526, bg=ORANGE, highlightthickness=0)
+        self.front_image = PhotoImage(file="images/front.png")
+        self.back_image = PhotoImage(file="images/back.png")
+        self.flashcard_image = self.flashcard.create_image(400, 263, image=self.front_image)
+        self.flashcard_language = self.flashcard.create_text(400, 180, text="Mandarin", font=("Arial", 15, "italic"))
+        self.flashcard_text = self.flashcard.create_text(400, 270, text=random.choice(self.data)['Mandarin'], font=("Arial", 22, "bold"))
+        self.flashcard.grid(column=0, row=0, columnspan=2)
+        self.flashcard.bind("<Button-1>", self.flip_flashcard)
 
-def flip_flashcard(event):
-    global ENGLISH_WORD
-    ENGLISH_WORD = RANDOM_WORD['English']       
-    if 150 < event.x < 635 and 100 < event.y < 415:
-        flashcard.itemconfig(flashcard_image, image=back_image)
-        flashcard.itemconfig(flashcard_text, text=ENGLISH_WORD, fill="White")
-        flashcard.itemconfig(flashcard_language, text="English", fill="White")
- 
-# ---------------------------- UI SETUP ------------------------------- #
-# Set up window
-window = Tk()
-window.title("Sino Flashcards")
-window.config(padx=50, pady=50, bg=ORANGE)
+        check_image = PhotoImage(file="images/check.png")
+        self.correct_button = Button(image=check_image, command=self.correct, bg=ORANGE, highlightthickness=0, bd=0, activebackground=ORANGE, highlightbackground=ORANGE)
+        self.correct_button.grid(column=1, row=1)
 
-# Front flashcard UI
-flashcard = Canvas(width=800, height=526, bg=ORANGE, highlightthickness=0)
-front_image = PhotoImage(file="images/front.png")
-back_image = PhotoImage(file="images/back.png")
-flashcard_image = flashcard.create_image(400, 263, image=front_image)
-flashcard_language = flashcard.create_text(400, 180, text="Mandarin", font=("Arial", 15, "italic"))
-flashcard_text = flashcard.create_text(400, 270, text=random.choice(data)['Mandarin'], font=("Arial", 22, "bold"))
-flashcard.grid(column=0, row=0, columnspan=2)
-flashcard.bind("<Button-1>", flip_flashcard)
-# Correct button
-check_image = PhotoImage(file="images/check.png")
-correct_button = Button(image=check_image, command=correct, bg=ORANGE, highlightthickness=0, bd=0, activebackground=ORANGE, highlightbackground=ORANGE)
-correct_button.grid(column=1, row=1)
+        wrong_image = PhotoImage(file="images/wrong.png")
+        self.incorrect_button = Button(image=wrong_image, command=self.update_flashcard, bg=ORANGE, highlightthickness=0, bd=0, activebackground=ORANGE, highlightbackground=ORANGE)
+        self.incorrect_button.grid(column=0, row=1)
+        
+        self.window.mainloop()
 
-# Incorrect Button
-wrong_image = PhotoImage(file="images/wrong.png")
-incorrect_button = Button(image=wrong_image, command=update_flashcard, bg=ORANGE, highlightthickness=0, bd=0, activebackground=ORANGE, highlightbackground=ORANGE)
-incorrect_button.grid(column=0, row=1)
+    def load_data(self):
+        try:
+            data = pandas.read_csv("data/unlearned_words.csv")
+        except:
+            data = pandas.read_csv("data/mandarin_words.csv")
+        return data.to_dict(orient='records')
 
+    def correct(self):
+        self.update_flashcard()
+        self.data.remove(self.random_word)
+        unlearned_words = pandas.DataFrame(self.data)
+        unlearned_words.to_csv("data/unlearned_words.csv", index=False)
 
+    def update_flashcard(self):
+        self.random_word = random.choice(self.data)
+        self.mandarin_word = self.random_word['Mandarin']
+        self.flashcard.itemconfig(self.flashcard_image, image=self.front_image)
+        self.flashcard.itemconfig(self.flashcard_language, text="Mandarin", fill="Black")
+        self.flashcard.itemconfig(self.flashcard_text, text=self.mandarin_word, fill="Black")
 
+    def flip_flashcard(self, event):
+        self.english_word = self.random_word['English']
+        if 150 < event.x < 635 and 100 < event.y < 415:
+            self.flashcard.itemconfig(self.flashcard_image, image=self.back_image)
+            self.flashcard.itemconfig(self.flashcard_text, text=self.english_word, fill="White")
+            self.flashcard.itemconfig(self.flashcard_language, text="English", fill="White")
 
-window.mainloop()
+flashcard = FlashcardApp()
